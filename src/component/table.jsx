e59844ee9ea95391c44c8c396ba14c8837ae1577
Ilkhomjon2604 <m347es@gmail.com>
2023-08-23 20:45:19 +0200

    delete is updated

--- src/component/table.jsx
+++ src/component/table.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import { users } from "./mock";
+import './table.css'
 
 class Table extends Component{
     constructor(props){
@@ -10,11 +11,22 @@ class Table extends Component{
         }
     }
     render(){
-
-
-
+        const onDelete = (id) =>{
+         const res = this.state.data.filter(val => val.id !== id)
+         this.setState({
+            data: res
+         })
+        }
         return(
-            <table>
+
+            <>
+            <div className="search-wrap">
+            <input className="search" type="text" name="filter" placeholder="type to search ..." />
+            </div>
+           
+
+            <table border={1} >
+
                 <thead>
                     <tr>
                         <th>ID</th>
@@ -22,27 +34,32 @@ class Table extends Component{
                         <th>Job</th>
                         <th>Status</th>
                         <th>Age</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    
-                        {this.state.data.map((value) =>{
+                        
+                        {this.state.data.length ?
+                        this.state.data.map((value, i) =>{
                             return (
-                                <tr>
-                                <td>{value.id}</td>
+                                <tr key={value.id}>
+                                <td >{i + 1}</td>
                                 <td>{value.name}</td>
                                 <td>{value.job}</td>
                                 <td>{value.status}</td>
                                 <td>{value.age}</td>
+                                <td><button onClick={()=>onDelete(value.id)}>Delete</button> <button>Edit</button></td>
                             </tr>
                             )
                             
                             
                             
-                        })}
+                        }): <tr > <th colSpan={6}>No Data Found</th></tr>}
             
                 </tbody>
             </table>
+
+            </>
         )
     }
 }
