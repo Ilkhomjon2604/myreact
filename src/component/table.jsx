3f27401ab86f7b1721588b24adb07da13fc0b6db
Ilkhomjon2604 <m347es@gmail.com>
2023-08-24 08:13:04 +0200

    search is added

--- src/component/table.jsx
+++ src/component/table.jsx
@@ -7,21 +7,87 @@ class Table extends Component{
         super(props);
         this.state = {
             data : users,
-            
+            name : '',
+            job : '',
+            status : '',
+            age : '',
+            search: 'id'
         }
     }
     render(){
+
+
+
         const onDelete = (id) =>{
-         const res = this.state.data.filter(val => val.id !== id)
+         let res = this.state.data.filter(val => val.id !== id)
          this.setState({
             data: res
-         })
+         });
+}
+        //  const onFilter = (e) => {
+        //     let result = users.filter(val => val.name.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()) ||  val.job.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()) ||  val.status.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()))
+        //     this.setState({
+        //         data : result
+        //     })
+        //  }
+         const onFilter = (e) => {
+            let result = users.filter(val => `${val[this.state.search]}`.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()) )
+            this.setState({
+                data : result
+            })
+         };
+
+         const onOption =(e)=>{
+            
+            this.setState({
+                search : e.target.value
+            })
+            console.log(this.state.search);
+         }
+
+        const creatObj = (e)=>{
+            this.setState({   [e.target.name] : e.target.value
+            })
+            
         }
+
+
+         const onAdd = () =>{
+
+            let newUser = {
+                id : users.length + 1,
+                name : this.state.name,
+                job: this.state.job,
+                status: this.state.status,
+                age : this.state.age
+            }
+            if(newUser.name.length && newUser.job.length && newUser.status.length){
+                this.setState({
+                    data : [...this.state.data, newUser],
+                    name : '',
+                    job: '',
+                    status: '',
+                    age: ''
+                })
+            }
+         
+         }
+         
+     
         return(
 
             <>
             <div className="search-wrap">
-            <input className="search" type="text" name="filter" placeholder="type to search ..." />
+
+                <select onChange={onOption} >
+                    <option value="id">ID</option>
+                    <option value="name">Name</option>
+                    <option value="job">Job</option>
+                    <option value="status">Status</option>
+                    <option value="age">Age</option>
+                </select>
+
+            <input  className="search" type="text" name="filter"  placeholder="type to search ..." onChange={onFilter} />
             </div>
            
 
@@ -43,12 +109,12 @@ class Table extends Component{
                         this.state.data.map((value, i) =>{
                             return (
                                 <tr key={value.id}>
-                                <td >{i + 1}</td>
+                                <td >{value.id}</td>
                                 <td>{value.name}</td>
                                 <td>{value.job}</td>
                                 <td>{value.status}</td>
                                 <td>{value.age}</td>
-                                <td><button onClick={()=>onDelete(value.id)}>Delete</button> <button>Edit</button></td>
+                                <td><button onClick={()=>onDelete(value.id)}>Delete</button> <button onClick={onAdd}>Edit</button></td>
                             </tr>
                             )
                             
@@ -58,7 +124,16 @@ class Table extends Component{
             
                 </tbody>
             </table>
+            
+            <div className="input-wrapper">
 
+                <input value={this.state.name} autoComplete="off" type="text" placeholder="Type in Your name..." name="name" onChange={creatObj}/>
+                <input value={this.state.job} autoComplete="off" type="text" placeholder="Type your occupation..." name="job" onChange={creatObj}/>
+                <input value={this.state.status} autoComplete="off" type="text" placeholder="Type your Status..." name="status" onChange={creatObj}/>
+                <input value={this.state.age} autoComplete="off" type="number" placeholder="What is your age..." name="age" onChange={creatObj}/>
+                <button type="submit" onClick={onAdd}>Add</button>
+                
+            </div>
             </>
         )
     }
