234d1d67fcc65aa45bf35f2e284f240302d1e2a7
Ilkhomjon2604 <m347es@gmail.com>
2023-09-05 02:26:11 +0200

    theme changer function

--- src/App.js
+++ src/App.js
@@ -1,49 +1,62 @@
 import React from 'react';
 import './App.css'
-import Sidebar from './components/sidebar/sidebar';
-import Body from './components/body/cards';
-import styled   from 'styled-components'
+import styled,  {ThemeProvider, createGlobalStyle} from 'styled-components'
+import { Wrapper, Title, HeadWrap, Number, FooterWrap, Name, Valid, Btn, Link } from './style';
+import logo from './chip.png'
 
-const Container = styled.div`
-display: flex;
-max-width: 1664px;
-width: 100%;
-flex-direction: column;
-background-color: #222;
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: ${(props)=> props.theme.bg};
+  color: ${(props)=> props.theme.cl}
+
+}
+.wrapper{
+  background-color: ${(props)=> props.theme.wrb} ;
+  
+}
+.btn{
+  background-color: ${(props)=> props.theme.btn};
+}
+.link{
+  color : ${(props)=> props.theme.link}
+}
 `
-
-
-
-
-const BodyWrapper = styled.div`
-  width: 100%;
-    display: flex;
-    justify-content: space-between;
-
-`
-
 
 class App extends React.Component {
   state = {
     light : true
   }
-  render(){
-   
-  
-
-  return (
-
-    <Container>
-
-      <BodyWrapper>
-        <Sidebar/>
-        <Body/>
-      </BodyWrapper>
-    </Container>
-
-  );
+ render(){
+  const theme ={
+    bg : this.state.light ? "white" : "black",
+    cl : this.state.light ? "black" : "white",
+    wrb : this.state.light ? "#ddd" : "#333",
+    btn : this.state.light ? "coral" : "blue",
+    link : this.state.light ? "red" : "blue"
   }
+  return (
+    <ThemeProvider theme ={theme}>
+      <GlobalStyle/>
+      <Wrapper className='wrapper'>
+        <HeadWrap>
+          <img src={logo} alt='img' width='70px'/>
+          <Title>Visa</Title>
+        </HeadWrap>
+        <Number>1435 5382 5928 0350</Number>
+
+        <FooterWrap>
+          <Name>Madewith React</Name>
+          <Valid>valid thru <br/> <strong>02/24</strong></Valid>
+        </FooterWrap>
+      </Wrapper>
+    <Btn className='btn' onClick={()=> this.setState({light : !this.state.light})}>Toggle Theme</Btn>
+    <Link href='https://github.com/Ilkhomjon2604' target='_blank' className='link'>Check my GitHub</Link>
+    </ThemeProvider>
+  )
+ }
+  
 }
+
 
 
 export default App;
