da9962ec012dd585e50fcfb0862cfd52e097f585
Ilkhomjon2604 <m347es@gmail.com>
2023-09-05 09:11:43 +0200

    crud with functional componnent

---
+++ src/components/mock.js
@@ -0,0 +1,12 @@
+export const users = [
+    {id:1, name:'Tom', job:'Student', status: 'Active', age : 20},
+    {id:2, name:'Hanna', job:'Student', status: 'Disactive', age : 18},
+    {id:3, name:'Ali', job:'Mantor', status: 'Active', age : 26},
+    {id:4, name:'Jone', job:'Front end', status: 'Active', age : 19},
+    {id:5, name:'Volt', job:'Student', status: 'Disactive', age : 22},
+    {id:6, name:'Smith', job:'Mantor', status: 'Active', age : 30},
+    {id:7, name:'Jony', job:'Student', status: 'Disactive', age : 18},
+    {id:8, name:'Adam', job:'Back end', status: 'Active', age : 28},
+    {id:9, name:'Anna', job:'Front end', status: 'Active', age : 19},
+    {id:10, name:'Beast', job:'Student', status: 'Active', age : 23}
+]
